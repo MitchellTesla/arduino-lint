359d29a85abf23dcaec4cd88d6dcd46694c5d2bd
Justin Mitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-05-13 01:47:16 -0400

    Update

---
+++ internal/project/packageindex/packageindex.go
@@ -0,0 +1,85 @@
+// This file is part of Arduino Lint.
+//
+// Copyright 2020 ARDUINO SA (http://www.arduino.cc/)
+//
+// This software is released under the GNU General Public License version 3,
+// which covers the main part of Arduino Lint.
+// The terms of this license can be found at:
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// You can be released from the requirements of the above licenses by purchasing
+// a commercial license. Buying such a license is mandatory if you want to
+// modify or otherwise use the software for commercial activities involving the
+// Arduino software without disclosing the source code of your own applications.
+// To purchase a commercial license, send an email to license@arduino.cc.
+
+/*
+Package packageindex provides functions specific to linting the package index files of the Arduino Boards Manager.
+See: https://arduino.github.io/arduino-cli/latest/package_index_json-specification
+*/
+package packageindex
+
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/arduino/go-paths-helper"
+)
+
+var empty struct{}
+
+// Reference: https://arduino.github.io/arduino-cli/latest/package_index_json-specification/#naming-of-the-json-index-file
+var validExtensions = map[string]struct{}{
+	".json": empty,
+}
+
+// HasValidExtension returns whether the file at the given path has a valid package index extension.
+func HasValidExtension(filePath *paths.Path) bool {
+	_, hasValidExtension := validExtensions[filePath.Ext()]
+	return hasValidExtension
+}
+
+// Regular expressions for official and non-official package index filenames
+// See: https://arduino.github.io/arduino-cli/latest/package_index_json-specification/#naming-of-the-json-index-file
+var validFilenameRegex = map[bool]*regexp.Regexp{
+	true:  regexp.MustCompile(`^package_(.+_)*index.json$`),
+	false: regexp.MustCompile(`^package_(.+_)+index.json$`),
+}
+
+// HasValidFilename returns whether the file at the given path has a valid package index filename.
+func HasValidFilename(filePath *paths.Path, officialRuleMode bool) bool {
+	regex := validFilenameRegex[officialRuleMode]
+	filename := filePath.Base()
+	return regex.MatchString(filename)
+}
+
+// Find searches the provided path for a file that has a name resembling a package index and returns the path to that file.
+func Find(folderPath *paths.Path) (*paths.Path, error) {
+	exist, err := folderPath.ExistCheck()
+	if !exist {
+		return nil, fmt.Errorf("Error opening path %s: %s", folderPath, err)
+	}
+
+	if folderPath.IsNotDir() {
+		return folderPath, nil
+	}
+
+	directoryListing, err := folderPath.ReadDir()
+	if err != nil {
+		return nil, err
+	}
+
+	directoryListing.FilterOutDirs()
+	for _, potentialPackageIndexFile := range directoryListing {
+		if HasValidFilename(potentialPackageIndexFile, true) {
+			return potentialPackageIndexFile, nil
+		}
+	}
+	for _, potentialPackageIndexFile := range directoryListing {
+		if HasValidExtension(potentialPackageIndexFile) {
+			return potentialPackageIndexFile, nil
+		}
+	}
+
+	return nil, nil
+}
---
+++ internal/project/packageindex/packageindex_test.go
@@ -0,0 +1,83 @@
+// This file is part of Arduino Lint.
+//
+// Copyright 2020 ARDUINO SA (http://www.arduino.cc/)
+//
+// This software is released under the GNU General Public License version 3,
+// which covers the main part of Arduino Lint.
+// The terms of this license can be found at:
+// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// You can be released from the requirements of the above licenses by purchasing
+// a commercial license. Buying such a license is mandatory if you want to
+// modify or otherwise use the software for commercial activities involving the
+// Arduino software without disclosing the source code of your own applications.
+// To purchase a commercial license, send an email to license@arduino.cc.
+
+package packageindex
+
+import (
+	"os"
+	"testing"
+
+	"github.com/arduino/go-paths-helper"
+	"github.com/stretchr/testify/assert"
+)
+
+var testDataPath *paths.Path
+
+func init() {
+	workingDirectory, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	testDataPath = paths.New(workingDirectory, "testdata")
+}
+
+func TestHasValidExtension(t *testing.T) {
+	assert.True(t, HasValidExtension(paths.New("/foo", "bar.json")))
+	assert.False(t, HasValidExtension(paths.New("/foo", "bar.baz")))
+}
+
+func TestHasValidFilename(t *testing.T) {
+	testTables := []struct {
+		testName         string
+		filename         string
+		officialRuleMode bool
+		assertion        assert.BoolAssertionFunc
+	}{
+		{"Official, primary", "package_index.json", true, assert.True},
+		{"Official, secondary", "package_foo_index.json", true, assert.True},
+		{"Official, invalid", "packageindex.json", true, assert.False},
+		{"Unofficial, valid", "package_foo_index.json", false, assert.True},
+		{"Unofficial, official", "package_index.json", false, assert.False},
+		{"Unofficial, invalid", "packageindex.json", false, assert.False},
+	}
+
+	for _, testTable := range testTables {
+		testTable.assertion(t, HasValidFilename(paths.New("/foo", testTable.filename), testTable.officialRuleMode), testTable.testName)
+	}
+}
+
+func TestFind(t *testing.T) {
+	testTables := []struct {
+		testName     string
+		path         *paths.Path
+		expectedPath *paths.Path
+		errAssertion assert.ValueAssertionFunc
+	}{
+		{"Nonexistent", testDataPath.Join("nonexistent"), nil, assert.NotNil},
+		{"File", testDataPath.Join("HasPackageIndex", "package_foo_index.json"), testDataPath.Join("HasPackageIndex", "package_foo_index.json"), assert.Nil},
+		{"Single", testDataPath.Join("HasPackageIndex"), testDataPath.Join("HasPackageIndex", "package_foo_index.json"), assert.Nil},
+		{"Multiple", testDataPath.Join("HasMultiple"), testDataPath.Join("HasMultiple", "package_foo_index.json"), assert.Nil},
+		{"Valid extension fallback", testDataPath.Join("HasJSON"), testDataPath.Join("HasJSON", "foo.json"), assert.Nil},
+		{"None", testDataPath.Join("HasNone"), nil, assert.Nil},
+	}
+
+	for _, testTable := range testTables {
+		path, err := Find(testTable.path)
+		testTable.errAssertion(t, err, testTable.testName)
+		if err == nil {
+			assert.Equal(t, testTable.expectedPath, path, testTable.testName)
+		}
+	}
+}
